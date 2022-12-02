22d24e82953feb1c615d88d7408cdace03fe278b
sukum <sukum@DESKTOP-P84MAU4>
2022-12-02 09:54:39 -0600

    changes done

--- src/main/java/com/acti/pages/TasksPage.java
+++ src/main/java/com/acti/pages/TasksPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.acti.base.DriverScript;
+import com.acti.utility.Helper;
 
 
 /*
@@ -23,9 +24,18 @@ public class TasksPage extends DriverScript {
 	@FindBy(xpath="//div[@class='item createNewCustomer']") WebElement newcustomeritem;
 	@FindBy(xpath = "(//input[@placeholder = 'Enter Customer Name'])[2]") WebElement customernameTextbox;
 	@FindBy(xpath = "//textarea[@placeholder = 'Enter Customer Description']") WebElement CustomerDescriptionTextBox;
-	@FindBy(linkText = "Create Customer") WebElement createCustomerButton;
+	@FindBy(xpath = "//div[contains(text(),'Create Customer')]") WebElement createCustomerButton;
 	@FindBy(xpath = "//span[@class='innerHtml']") WebElement SuccessMessage;
-	
+	@FindBy(xpath="(//input[@placeholder='Start typing name ...'])[1]") WebElement textboxStartTyping;
+	@FindBy(xpath="//div[@class='filteredContainer']//div[@class='title']") WebElement searchedCustomer;
+	@FindBy(xpath="//div[@class='titleEditButtonContainer']//div[@class='editButton']") WebElement buttonEdit;
+	@FindBy(xpath="//div[@class='editCustomerPanelHeader']//div[@class='action'][normalize-space()='ACTIONS']" )
+	WebElement buttonAction;
+	@FindBy(xpath="//div[@class='taskManagement_customerPanel']//div[@class='title'][normalize-space()='Delete']")
+	WebElement buttonDelete;
+	@FindBy(xpath="(//span[@class='submitTitle buttonTitle'])[1]") WebElement buttonDeletePermanently;
+
+
     
 	// ********************************Page Initialization*********************************//
 
@@ -36,6 +46,42 @@ public class TasksPage extends DriverScript {
 	}
 
 	// ********************************Page Method/Actions*********************************//
+	
+
+	public void enterSearchCustomer(String custname)
+	{
+		textboxStartTyping.sendKeys(custname);
+	}
+	
+	public void clickDeletePermanently()
+	{
+		buttonDeletePermanently.click();
+		Helper.sleep();
+	}
+	
+	public void clickDelete()
+	{
+		buttonDelete.click();
+	}
+	
+	public void clickAction()
+	{
+		buttonAction.click();
+		Helper.sleep();
+	}
+	
+	public void clickEditButton()
+	{
+		buttonEdit.click();
+		Helper.sleep();
+	}
+	
+	public void clickSearchedCustomer()
+	{
+		searchedCustomer.click();
+	}
+	
+		
 	
 	public void ClickAddNewwButton ()
 	{
