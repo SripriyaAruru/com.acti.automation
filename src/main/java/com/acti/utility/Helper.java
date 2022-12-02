1a5b2016f6edcb2f87826af026fea62588b1123a
sukum <sukum@DESKTOP-P84MAU4>
2022-12-02 10:01:31 -0600

    added sleep

--- src/main/java/com/acti/utility/Helper.java
+++ src/main/java/com/acti/utility/Helper.java
@@ -1,5 +1,18 @@
 package com.acti.utility;
 
 public class Helper {
+	public static void sleep()
+	{
+		try 
+		{
+			Thread.sleep(2000);
+		} 
+		catch (InterruptedException e) 
+		{	
+			e.printStackTrace();
+		}
+	}
 
 }
+
+
