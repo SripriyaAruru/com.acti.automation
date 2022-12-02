52970c8bbf8becbb19c3547549b2220b3f7c4a44
sukum <sukum@DESKTOP-P84MAU4>
2022-12-02 10:00:50 -0600

    done changes

--- src/test/java/com/acti/testcase/TaskPageTest.java
+++ src/test/java/com/acti/testcase/TaskPageTest.java
@@ -1,5 +1,8 @@
 package com.acti.testcase;
 
+
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TaskPageTest extends BaseTest {
@@ -11,26 +14,31 @@ public class TaskPageTest extends BaseTest {
 		lp.enterPassword(Password);
 		lp.Clicklogin();
 		ep.clickTaskMenu();
+		tp.ClickAddNewwButton();		
 		tp.clicknewcustomeritem();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		tp.Entercustomernametextbox("TestCustomer");
+		tp.EntercustomerDescription("Dummy Customer created for testing ");
+		tp.ClickcreateCustomerButton();
+		String sesuccessmessage = tp.getSuccessMessage();
+		Assert.assertTrue(sesuccessmessage.contains(" has been created"));
+		ep.ClickLogout();
+	}
+	@Test (dataProvider = "actilogin")
+	public void testDeleteCustomer (String Username, String Password )
+	{
+
+		lp.enterUsername(Username);
+		lp.enterPassword(Password);
+		lp.Clicklogin();
+		ep.clickTaskMenu();
+		tp.enterSearchCustomer("TestCustomer");
+		tp.clickSearchedCustomer();
+		tp.clickEditButton();
+		tp.clickAction();
+		tp.clickDelete();
+		tp.clickDeletePermanently();
+		String sesuccessmessage = tp.getSuccessMessage();
+		Assert.assertTrue(sesuccessmessage.contains(" has been deleted"));
 		ep.ClickLogout();
 	}
 
